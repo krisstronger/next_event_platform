2c6d3f10a681b96898bf22c8164f55fa60d56dfa
kriss <ksandoval@cerveros.cl>
2023-12-27 14:14:21 -0300

    add dropDown menu UI component2

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,50 +1,42 @@
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { Button } from '../ui/button'
-import NavItems from './NavItems'
-import MobileNav from './MobileNav'
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
-    <header className='w-full border-b'>
-        <div className='wrapper flex items-center justify-between'>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-            <Image
-                src="/assets/images/logo.svg"
-                width={128}
-                height={38}
-                alt="logo"
-                className=""/>
+          <Image 
+            src="/assets/images/logo.svg" width={128} height={38}
+            alt="Evently logo" 
+          />
         </Link>
 
         <SignedIn>
-            <nav className='md:flex-between hidden w-full max-w-xs'>
-                <MobileNav></MobileNav>
-            </nav>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
         </SignedIn>
 
-
-        <div className='flex w-32 justify-end gap-3'>
-        <SignedIn>
-                <UserButton afterSignOutUrl='/'>
-                   <NavItems></NavItems>
-                </UserButton>
-            </SignedIn>
-
-            <SignedOut>
-                <Button asChild className="rounded-full" size="lg">
-                    <Link href="/sign-in">
-                        Login
-                    </Link>
-                </Button>
-            </SignedOut>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
-
-
-        </div>
-        </header>
+      </div>
+    </header>
   )
 }
 
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { headerLinks } from '@/constants'
 import Link from 'next/link'
@@ -6,27 +6,24 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const NavItems = () => {
-    const pathName = usePathname();
+  const pathname = usePathname();
 
   return (
-    <ul className='md:flex-between flex w-full flex-col items-start gep-5 md:flex-row'>
-        {
-            headerLinks.map( (link) => {
-                const isActive = pathName === link.route;
-
-
-                return (
-                    <li
-                    key={link.route}
-                    className={`${
-                      isActive && 'text-primary-500'
-                    } flex-center p-medium-16 whitespace-nowrap`}
-                  >
-                        <Link href={link.route}>{link.label}</Link>
-                    </li>
-                )
-            })
-        }
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        const isActive = pathname === link.route;
+        
+        return (
+          <li
+            key={link.route}
+            className={`${
+              isActive && 'text-primary-500'
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        )
+      })}
     </ul>
   )
 }
