b4c312c7f88fa4b84dafec42598ac216b6dc666d
kriss <ksandoval@cerveros.cl>
2023-12-27 10:13:27 -0300

    Comment checkoutButton in root2

--- components/shared/DeleteConfirmation.tsx
+++ components/shared/DeleteConfirmation.tsx
@@ -18,39 +18,37 @@ import {
 
 import { deleteEvent } from '@/lib/actions/event.actions'
 
-const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
-    const pathname = usePathname()
-    let [isPending, startTransition] = useTransition();
+export const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
+  const pathname = usePathname()
+  let [isPending, startTransition] = useTransition()
 
-    return (
-        <AlertDialog>
-          <AlertDialogTrigger>
-            <Image src="/assets/icons/delete.svg" alt="edit" width={20} height={20} />
-          </AlertDialogTrigger>
-    
-          <AlertDialogContent className="bg-white">
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you sure you want to delete?</AlertDialogTitle>
-              <AlertDialogDescription className="p-regular-16 text-grey-600">
-                This will permanently delete this event
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-    
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-    
-              <AlertDialogAction
-                onClick={() =>
-                  startTransition(async () => {
-                    await deleteEvent({ eventId, path: pathname })
-                  })
-                }>
-                {isPending ? 'Deleting...' : 'Delete'}
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      )
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Image src="/assets/icons/delete.svg" alt="edit" width={20} height={20} />
+      </AlertDialogTrigger>
+
+      <AlertDialogContent className="bg-white">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to delete?</AlertDialogTitle>
+          <AlertDialogDescription className="p-regular-16 text-grey-600">
+            This will permanently delete this event
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+
+          <AlertDialogAction
+            onClick={() =>
+              startTransition(async () => {
+                await deleteEvent({ eventId, path: pathname })
+              })
+            }>
+            {isPending ? 'Deleting...' : 'Delete'}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
 }
-
-export default DeleteConfirmation
